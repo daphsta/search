b2974c258e61a10d4cc45f7e3bde52c777aa23f2
Daphne <yiingshan@gmail.com>
2019-03-11 00:51:42 +1100

    Refactor search engine
    Extract method to find search value in nested records

--- lib/search/search_engine.rb
+++ lib/search/search_engine.rb
@@ -7,17 +7,23 @@ class SearchEngine
 
   def search(search_key:, search_value:)
     converted_search_value = [ SearchValueTypes::Converter.call(search_value) ]
-    
+
     repository.fetch_all.select do |hash_data|
       next unless hash_data.has_key?(search_key)
 
       matched_value = hash_data[search_key]
 
-      if matched_value.is_a?(Array)
-        matched_value.any? { |v| converted_search_value.include?(v) }
-      else
-        converted_search_value.include?(matched_value)
-      end
+      find_value_in_nested(matched_value, converted_search_value)
+    end
+  end
+
+  private
+
+  def find_value_in_nested(matched_value, converted_search_value)
+    if matched_value.is_a?(Array)
+      matched_value.any? { |v| converted_search_value.include?(v) }
+    else
+      converted_search_value.include?(matched_value)
     end
   end
 end
